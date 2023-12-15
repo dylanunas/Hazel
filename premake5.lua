a3ec0e19e97326ba7e068b10cba3a4b040279967
Dylan Unas <dylanunas@gmail.com>
2023-12-15 18:13:20 -0500

    Added premake to set project up easier

---
+++ premake5.lua
@@ -0,0 +1,92 @@
+workspace "Blink"
+	architecture "x64"
+	configurations { "Debug", "Release", "Dist" }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Blink"
+	location "Blink"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files { 
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22621.0"
+
+		defines {
+			"BLINK_PLATFORM_WINDOWS",
+			"BLINK_BUILD_DLL"
+		}
+
+		postbuildcommands {
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
+		}
+
+	filter "configurations:Debug"
+		defines "BLINK_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BLINK_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "BLINK_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files { 
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"Blink/vendor/spdlog/include",
+		"Blink/src"
+	}
+
+	links {
+		"Blink"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22621.0"
+
+		defines {
+			"BLINK_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "BLINK_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BLINK_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "BLINK_DIST"
+		optimize "On"
+
